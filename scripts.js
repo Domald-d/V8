11970d6a805cb4743b8acca914c31f210d403e6f
Dommz <Dommz@hotmail.com>
2024-10-17 18:50:25 +0000

    föll uppsett í sitthvora skrá

--- scripts.js
+++ scripts.js
@@ -7,6 +7,12 @@
  */
 
 import { isString, splitOnWhitespace } from './lib/helpers.js';
+import { palindrome } from './lib/palindrome.js';
+import { longest } from './lib/longest.js';
+import { shortest } from './lib/shortest.js';
+import { reverse } from './lib/reverse.js';
+import { vowels } from './lib/vowel.js';
+import { consonants } from './lib/consonant.js'
 
 const test = isString('hæ');
 console.log('test er strengur?', test);
@@ -16,3 +22,9 @@ const stringWithWhitespace = `halló
 hæ`;
 const split = splitOnWhitespace(stringWithWhitespace);
 console.log(split);
+const  test1 = palindrome(str);
+const test2 = longest(str);
+const test3 = shortest(str);
+const test4 = reverse(str);
+const test5 = vowels(str);
+const test6 = consonants(str);
