24cf87ff01c56dd30e7633f95b687b3bccb4fd45
Dommz <Dommz@hotmail.com>
2024-10-21 18:10:28 +0000

    bæta build möppu

--- build/lib/longest.js
+++ build/lib/longest.js
@@ -1,7 +1,7 @@
-import { isString, splitOnWhitespace} from './helpers.js';
+import { isString} from './helpers.js';
 export function longest(str) {
     if(isString(str)){
-      const split = splitOnWhitespace(str);
+      const split = str.split(' ');
       let staerst = split[0];
       for(let i = 1; i < split.length; i++){
         if(split[i].length > staerst.length){
--- build/lib/reverse.js
+++ build/lib/reverse.js
@@ -1,7 +1,7 @@
-import { isString, splitOnWhitespace } from "./helpers.js";
+import { isString} from "./helpers.js";
 export function reverse(str) {
     if(isString(str)){
-      const split = splitOnWhitespace(str);
+      const split = str.split('');
       const reversed = split.reverse();
     
       return reversed.join('')
